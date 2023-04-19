26ae04b7c411fb96ebbd8d720e30205366ec8673
Ben Fields <fieldsben5@gmail.com>
2023-04-19 03:34:59 -0700

    final little fixes

--- Chaos/Chaos.cpp
+++ Chaos/Chaos.cpp
@@ -8,7 +8,7 @@
 using namespace sf;
 using namespace std;
 
-CircleShape getPosBetweenPoints(CircleShape current, CircleShape vertex, float ratio, Color frameColor)
+CircleShape getPosBetweenPoints(CircleShape current, CircleShape vertex, float ratio)
 {
 	float x1 = current.getPosition().x;
 	float y1 = current.getPosition().y;
@@ -19,15 +19,10 @@ CircleShape getPosBetweenPoints(CircleShape current, CircleShape vertex, float r
 	float x3 = x1 + ((x2 - x1) * ratio);
 	float y3 = y1 + ((y2 - y1) * ratio);
 
-	CircleShape z;
-	z.setRadius(1);
-	z.setFillColor(frameColor);
-	z.setOutlineColor(frameColor);
-	z.setOutlineThickness(1);
-	z.setPosition(x3, y3);
+	CircleShape newPoint;
+	newPoint.setPosition(x3, y3);
 
-	return z;
-
+	return newPoint;
 }
 
 int main()
@@ -45,14 +40,13 @@ int main()
 	float height = sf::VideoMode::getDesktopMode().height;
 
 	float ratio;
-	int numPoints = 0;
+	int numVertices = 0;
 	int colorCounter = 0;
 	int frameCounter = 0;
 
-	vector <CircleShape> points;
-
 	CircleShape currentPoint;
 	CircleShape point;
+	vector <CircleShape> points;
 
 	Color rainbow[6] = { Color::Red, Color::Yellow, Color::Green, Color::Cyan, Color::Blue, Color::Magenta };
 	Color frameColor = rainbow[colorCounter];
@@ -67,10 +61,12 @@ int main()
 	*/
 
 	Font font;
-	font.loadFromFile("fonts/KOMIKAP_.ttf");
+	if (!font.loadFromFile("fonts/KOMIKAP_.ttf"))
+	{
+		cout << "couldnt find font..." << endl;
+	}
 
 	Text text;
-	//text.setPosition(width / 3, 0);
 	text.setFont(font);
 	text.setString("- Left click to place vertices\n- Then right click to start the game.\n- Press Esc to quit.");
 	text.setCharacterSize(20);
@@ -121,7 +117,7 @@ int main()
 				{
 					if (!inputFinished)
 					{
-						numPoints++;
+						numVertices++;
 
 						point.setRadius(1);
 						point.setOutlineColor(frameColor);
@@ -129,15 +125,14 @@ int main()
 						point.setOutlineThickness(1);
 						point.setPosition(event.mouseButton.x, event.mouseButton.y);
 						points.push_back(point);
-
 					}
 				}
 				else if (event.mouseButton.button == Mouse::Right)
 				{
 					if (!inputFinished)
 					{
-						ratio = static_cast<float>(numPoints) / (numPoints + 3);
 						inputFinished = true;
+						ratio = static_cast<float>(numVertices) / (numVertices + 3);
 
 						point.setRadius(1);
 						point.setOutlineColor(frameColor);
@@ -164,7 +159,33 @@ int main()
 		window.clear();
 
 		window.draw(text);
-		
+
+		// Generate new points
+		if (inputFinished)
+		{
+			for (int i = 0; i < POINTS_PER_FRAME; i++)
+			{
+				/*Current point is the most recently generated one*/
+				currentPoint = points.back();
+
+				/*Choose a random vertex (the initial points)*/
+				int randInt = rand() % numVertices;
+				CircleShape randVertex = points.at(randInt);
+
+				/* Make a new Circleshape and calculate it's position between 
+				the current point and the random vertex */
+				CircleShape newPoint;
+				newPoint = getPosBetweenPoints(currentPoint, randVertex, ratio);
+				newPoint.setRadius(1);
+				newPoint.setFillColor(frameColor);
+				newPoint.setOutlineColor(frameColor);
+				newPoint.setOutlineThickness(1);
+
+				/* Add the new point to the points vector*/
+				points.push_back(newPoint);
+			}
+		}
+
 		// Update color
 		if (frameCounter % 20 == 0)
 		{
@@ -181,33 +202,11 @@ int main()
 				points.at(i).setOutlineColor(frameColor);
 			}
 		}
-		
 
-		// TODO: Speed this up somehow??
+		// Draw points
 		for (unsigned int i = 0; i < points.size(); i++)
 		{
 			window.draw(points.at(i));
-		}
-
-		if (inputFinished)
-		{
-			for (int i = 0; i < POINTS_PER_FRAME; i++)
-			{
-				/*Current point is the most recently generated one*/
-				currentPoint = points.back();
-
-				/*Choose a random vertex (the initial points)*/
-				int randInt = rand() % numPoints;
-				CircleShape randVertex = points.at(randInt);
-
-				/* Make a new Circleshape and calculate it's position between 
-				the current point and the random vertex */
-				CircleShape newPoint;
-				newPoint = getPosBetweenPoints(currentPoint, randVertex, ratio, frameColor);
-
-				/* Add the new point to the points vector*/
-				points.push_back(newPoint);
-			}
 		}
 
 		frameCounter++;
